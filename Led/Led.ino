92bafbc8d271834537e0c0e865cf4d4c6882713d
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-22 22:17:34 +0200

    led

---
+++ Led/Led.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(13,OUTPUT);
+  /*digitalWrite(13,HIGH);
+  delay(5000);
+  digitalWrite(13,LOW);
+  delay(5000);*/
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  
+
+}
